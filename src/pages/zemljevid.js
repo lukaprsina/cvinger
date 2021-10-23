3d478de0b230aa793afa6cc2b0cae1938b19aae5
Luka Pršina <prsinaluka@gmail.com>
2021-10-23 14:38:06 +0200

    from linux

--- src/pages/zemljevid.js
+++ src/pages/zemljevid.js
@@ -1,27 +1,11 @@
-import React from "react"
-import { MapContainer, ImageOverlay } from "react-leaflet"
-import { LatLngBounds } from "leaflet"
+import React, { useEffect } from "react"
+
 import zemljevid from "../images/zemljevid/zemljevid.jpg"
 import Article from "../components/Article"
 
-function MapPlaceholder() {
-    return (
-        <p>
-            <noscript>You need to enable JavaScript to see this map.</noscript>
-        </p>
-    )
-}
-
 const Zemljevid = () => {
-    const bounds = new LatLngBounds(
-        [40.712216, -74.22655],
-        [40.773941, -74.12544]
-    )
     return (
         <Article title="Zemljevid">
-            {/* <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
-                <ImageOverlay url={zemljevid} bounds={bounds} />
-            </MapContainer> */}
         </Article>
     )
 }

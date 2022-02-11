1229047d0c32658242ebf1d4173882587ecc8e0e
Luka Pršina <luk.prsina@gmail.com>
2022-02-11 10:05:06 +0100

    scroll works, before pinch

--- pages/zemljevid.tsx
+++ pages/zemljevid.tsx
@@ -1,48 +1,82 @@
-import React, { useRef } from "react"
+import React, { useState } from "react"
 
 import Article from "../components/Article"
-import { useSpring, animated } from "react-spring"
+import { useSpring, animated, to } from "react-spring"
 import { Container } from "@mui/material"
 import useEventListener from "../components/useEventListener";
-import { Box } from "@mui/system"
-import { useDrag } from '@use-gesture/react'
+import { useGesture } from '@use-gesture/react'
 
 import zemljevid from "/public/images/zemljevid/zemljevid.jpg"
 
 const Zemljevid = () => {
-    const preventDefault = (e: Event) => e.preventDefault()
-    const domTarget = useRef()
-    const [position, setPosition] = useSpring(() => ({ x: 0, y: 0 }))
+    const maxZoom = 5
+    const minZoom = 0
+    const deltaZoom = 0.5
 
-    useEventListener("gesturestart", preventDefault)
-    useEventListener("gesturechange", preventDefault)
-
-    const bind = useDrag(({ event, down, movement: [mx, my] }) => {
-        event.preventDefault()
-    })
-
-    const styles = useSpring({
-        from: {
+    const { x, y, scale, zoom } = useSpring({
+        to: {
+            scale: 1,
+            zoom: 0,
             x: 0,
             y: 0,
         },
-        to: {
-            x: position.x,
-            y: position.y,
-        },
-        delay: 1000,
     })
 
+    let pos = { x: 0, y: 0 }
+
+    const preventDefault = (e: Event) => e.preventDefault()
+    useEventListener("gesturestart", preventDefault)
+    useEventListener("gesturechange", preventDefault)
+    const myRef = React.useRef(null)
+
+    const bind = useGesture({
+        onDrag: ({ event, delta: [mx, my] }) => {
+            event.preventDefault()
+            pos = { x: pos.x + mx * 0.5, y: pos.y + my * 0.5 }
+            x.set(pos.x)
+            y.set(pos.y)
+        },
+        onWheel: ({ event, delta, last }) => {
+            event.preventDefault()
+            pos.x -= delta[0]
+            x.set(pos.x)
+
+            if (last)
+                return
+            const zoomLevel = zoom.get()
+            if (delta[1] > 0) {
+                if (zoomLevel > minZoom) {
+                    zoom.set(zoomLevel - deltaZoom)
+                }
+            } else if (delta[1] < 0) {
+                if (zoomLevel < maxZoom) {
+                    zoom.set(zoomLevel + deltaZoom)
+                }
+            }
+            console.log(zoomLevel)
+
+        },
+        onPinch: ({ offset: [mx, my] }) => {
+            console.log("pinch", mx, my)
+        },
+    }, {
+        target: myRef,
+        eventOptions: { passive: false }
+    })
 
     return (
         <Article title="Zemljevid">
             <Container fixed sx={{
                 overflow: "hidden",
             }}>
-                <Box
-                    ref={domTarget}
-                    {...bind()}
-                    sx={{
+                <animated.div
+                    // {...bind()}
+                    ref={myRef}
+                    style={{
+                        x,
+                        y,
+                        width: zemljevid.width,
+                        height: zemljevid.height,
                         touchAction: "none",
                     }}
                 >
@@ -51,11 +85,13 @@ const Zemljevid = () => {
                         width={zemljevid.width}
                         height={zemljevid.height}
                         alt="zemljevid"
-                        style={styles}
+                        style={{
+                            scale: to([zoom, scale], (s, z) => s + z),
+                        }}
                     />
-                </Box>
+                </animated.div>
             </Container>
-        </Article>
+        </Article >
     )
 }
 
